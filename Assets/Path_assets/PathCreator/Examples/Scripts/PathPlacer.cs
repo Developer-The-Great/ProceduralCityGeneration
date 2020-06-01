619bbc203ac5d4c230e51cc06611120840cbf29c
RaphaelPriatama <blue.person16@gmail.com>
2020-06-01 13:12:00 +0200

    art1

---
+++ Assets/Path_assets/PathCreator/Examples/Scripts/PathPlacer.cs
@@ -0,0 +1,52 @@
+﻿using PathCreation;
+using UnityEngine;
+
+namespace PathCreation.Examples {
+
+    [ExecuteInEditMode]
+    public class PathPlacer : PathSceneTool {
+
+        public GameObject prefab;
+        public GameObject holder;
+        public float spacing = 3;
+
+        const float minSpacing = .1f;
+
+        //Change the Quaternion values depending on the values of the Sliders
+       
+
+        void Generate () {
+            if (pathCreator != null && prefab != null && holder != null) {
+                DestroyObjects ();
+
+                VertexPath path = pathCreator.path;
+
+                spacing = Mathf.Max(minSpacing, spacing);
+                float dst = 0;
+
+                while (dst < path.length) {
+                    Vector3 point = path.GetPointAtDistance (dst);
+                    // Quaternion rot = path.GetRotationAtDistance (dst);
+                    //Quaternion rot = ;
+                    Quaternion rot = new Quaternion();
+                    rot.Set(0, 0, 0, 1);
+                    Instantiate (prefab, point, rot, holder.transform);
+                    dst += spacing;
+                }
+            }
+        }
+
+        void DestroyObjects () {
+            int numChildren = holder.transform.childCount;
+            for (int i = numChildren - 1; i >= 0; i--) {
+                DestroyImmediate (holder.transform.GetChild (i).gameObject, false);
+            }
+        }
+
+        protected override void PathUpdated () {
+            if (pathCreator != null) {
+                Generate ();
+            }
+        }
+    }
+}
